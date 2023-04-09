5fc959e99d0723365ce5157e1378bd8b125fbab4
hoshi005 <hoshi005@me.com>
2023-04-09 09:14:58 +0900

    Initial Commit

---
+++ qr-code/qr_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  qr_codeApp.swift
+//  qr-code
+//
+//  Created by Susumu Hoshikawa on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct qr_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
